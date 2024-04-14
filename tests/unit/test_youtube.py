f3aea8b2b0bae2b77c0b0884cc943cf0533a351d
Christian Boin <christianboin@hotmail.com>
2024-04-14 17:21:59 -0400

    feat: add youtube dl tests and mock out web tests

--- tests/unit/test_youtube.py
+++ tests/unit/test_youtube.py
@@ -24,3 +24,22 @@ def test_run_download(video_url: str, fmt: str):
         file_format=fmt
     )
     assert res is not None
+
+def test_get_video_links():
+    youtube_home_url: str ="https://www.youtube.com"
+    youtube_search_url: str = "https://www.youtube.com/results"
+    youtube_search_payload: dict = {"search_query": "jolene dolly parton"}
+
+    link_list, links = youtube.get_video_links(
+        youtube_home_url=youtube_home_url,
+        youtube_search_url=youtube_search_url,
+        youtube_query_payload=youtube_search_payload,
+        render_timeout=20,
+        render_wait=0.2,
+        retry_count=3,
+        render_sleep=1
+    )
+
+    assert len(link_list) > 0 and len(links) > 0
+
+
